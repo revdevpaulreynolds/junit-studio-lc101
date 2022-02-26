1a102196c6a25c69778b12878d659a4a8e8e8f2b
Paul Reynolds <usuallypaulreynolds@gmail.com>
2022-02-25 22:46:32 -0600

    Passed bonus mission on chapter 6.

--- src/test/BalancedBracketsTest.java
+++ src/test/BalancedBracketsTest.java
@@ -1,5 +1,6 @@
 package test;
 
+import main.BonusBinarySearch;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
@@ -7,10 +8,11 @@ import static org.junit.Assert.*;
 public class BalancedBracketsTest {
 
     //TODO: add tests here
+
+
     @Test
-    public void emptyTest() {
-        assertEquals(true, true);
+    public void thisArray() {
+        int[] thisArray =  {1, 3, 5, 7, 9};
+        assertEquals(4, BonusBinarySearch.binarySearch(thisArray, 9));
     }
-
-
 }
